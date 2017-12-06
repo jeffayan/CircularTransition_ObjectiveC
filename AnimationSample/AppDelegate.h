2d4be45ecd3d5ec4a14756bac0337137749714ab
raviteja11b <raviteja.buragadda@appface.in>
2017-12-06 18:25:02 +0530

    Initial Commit

---
+++ AnimationSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationSample
+//
+//  Created by imac on 12/6/17.
+//  Copyright © 2017 Datum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationSample
+//
+//  Created by imac on 12/6/17.
+//  Copyright © 2017 Datum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
